a0dec99f42eaf0df410b39247fcd1fa7cc4ed065
Chris LaRose <cjlarose@gmail.com>
2022-06-06 15:24:40 -0700

    Add neovim

--- home.nix
+++ home.nix
@@ -4,4 +4,11 @@
     userName = "Chris LaRose";
     userEmail = "cjlarose@gmail.com";
   };
+
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+  };
 }
