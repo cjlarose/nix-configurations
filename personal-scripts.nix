2d46895e00c881779182da031ae4bf2926be8356
Chris LaRose <cjlarose@gmail.com>
2022-06-07 18:10:48 +0000

    Wrap rg results with pager

--- personal-scripts.nix
+++ personal-scripts.nix
@@ -8,8 +8,21 @@ let
       nvr -cc split --remote-wait +'set bufhidden=wipe' "$@"
     fi
   '';
+
+  wrappedRg = pkgs.writeShellScriptBin "rg" ''
+    if [ -t 1 ]; then
+      ${pkgs.ripgrep}/bin/rg --pretty --sort path "$@" | less
+    else
+      ${pkgs.ripgrep}/bin/rg --sort path "$@"
+    fi
+  '';
+  linkedRg = pkgs.symlinkJoin {
+    name = "rg";
+    paths = [ wrappedRg pkgs.ripgrep ];
+  };
 in {
   home.packages = [
     nvrEditInSplitWindow
+    linkedRg
   ];
 }
