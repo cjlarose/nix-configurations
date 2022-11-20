f0d562184cf1ed883f2c28215bf4c7d06f75609e
Chris LaRose <cjlarose@gmail.com>
2022-11-20 23:55:38 +0000

    Add new configuration for router

--- flake.nix
+++ flake.nix
@@ -16,6 +16,86 @@
   };
 
   outputs = { self, nixpkgs, darwin, home-manager, fzfVim, fzfProject, pinpox }: {
+    nixosConfigurations."router" = nixpkgs.lib.nixosSystem (
+      let
+        system = "x86_64-linux";
+      in {
+        inherit system;
+        modules = [
+          ({ pkgs, ... }: {
+            imports = [ ./hardware-configuration-router.nix ];
+
+            networking.hostName = "router";
+
+            boot.loader.systemd-boot.enable = true;
+            boot.loader.efi.canTouchEfiVariables = true;
+
+            system.stateVersion = "22.05";
+
+            nix = {
+              package = pkgs.nixFlakes;
+              extraOptions = ''
+                experimental-features = nix-command flakes
+              '';
+              registry.nixpkgs.flake = nixpkgs;
+            };
+
+            nixpkgs.overlays = [
+              fzfVim.overlay
+              fzfProject.overlay
+            ];
+
+            security.sudo.wheelNeedsPassword = false;
+            security.pam.loginLimits = [
+              {
+                domain = "*";
+                type = "soft";
+                item = "nofile";
+                value = "65536";
+              }
+            ];
+
+            environment.systemPackages = with pkgs; [
+              lsof
+              iotop
+            ];
+
+            services.openssh = {
+              enable = true;
+              passwordAuthentication = false;
+              permitRootLogin = "no";
+            };
+
+            programs.ssh.startAgent = true;
+
+            programs.zsh.enable = true;
+
+            users.mutableUsers = false;
+
+            users.users.cjlarose = {
+              isNormalUser = true;
+              home = "/home/cjlarose";
+              extraGroups = [ "docker" "wheel" ];
+              shell = pkgs.zsh;
+              hashedPassword = "$6$YLrfXTwu61JGE.v8$kR5ZdMso2lcnyy7s7GXkIb.kLDyQ2UW3aDyGerQYni96g2kPC1MIY48Y9Q3SdYe2ycuVCrKgH6DlOjUUsK02s0";
+              openssh.authorizedKeys.keys = [
+                "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGFtA/9w60OssA+Eji+Ygvd1XCJk/zw/uYLdiiaevELu cjlarose"
+              ];
+            };
+          })
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.cjlarose = import ./home.nix;
+            home-manager.extraSpecialArgs = {
+              inherit system pinpox;
+              server = true;
+            };
+          }
+        ];
+      }
+    );
+
     nixosConfigurations."pt-dev" = nixpkgs.lib.nixosSystem (
       let
         system = "x86_64-linux";
