883bb1a76361c7d17dd6377505fe41e2e304790a
Chris LaRose <cjlarose@gmail.com>
2023-04-08 19:07:27 -0700

    Move darwin configurations to new module

---
+++ darwin-configurations/default.nix
@@ -0,0 +1,40 @@
+{ nixpkgs, darwin, home-manager, fzfVim, fzfProject, pinpox }:
+let
+  system = "x86_64-darwin";
+in {
+  "LaRose-MacBook-Pro" = darwin.lib.darwinSystem {
+    inherit system;
+    modules = [
+      ({ pkgs, ... }: {
+        environment.systemPackages = [
+          pkgs.vim
+        ];
+        environment.etc = {
+          "sysctl.conf" = {
+            enable = true;
+            text = ''
+              kern.maxfiles=131072
+              kern.maxfilesperproc=65536
+            '';
+          };
+        };
+        services.nix-daemon.enable = true;
+        programs.zsh.enable = true;
+        system.stateVersion = 4;
+        nixpkgs.overlays = [
+          fzfVim.overlay
+          fzfProject.overlay
+        ];
+      })
+      home-manager.darwinModules.home-manager {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.users.chrislarose = import ../home.nix;
+        home-manager.extraSpecialArgs = {
+          inherit system pinpox;
+          server = false;
+        };
+      }
+    ];
+  };
+}
--- flake.nix
+++ flake.nix
@@ -21,44 +21,9 @@
         inherit nixpkgs home-manager fzfVim fzfProject pinpox;
       }
     );
-
-    darwinConfigurations."LaRose-MacBook-Pro" = darwin.lib.darwinSystem (
-      let
-        system = "x86_64-darwin";
-      in {
-        inherit system;
-        modules = [
-          ({ pkgs, ... }: {
-            environment.systemPackages = [
-              pkgs.vim
-            ];
-            environment.etc = {
-              "sysctl.conf" = {
-                enable = true;
-                text = ''
-                  kern.maxfiles=131072
-                  kern.maxfilesperproc=65536
-                '';
-              };
-            };
-            services.nix-daemon.enable = true;
-            programs.zsh.enable = true;
-            system.stateVersion = 4;
-            nixpkgs.overlays = [
-              fzfVim.overlay
-              fzfProject.overlay
-            ];
-          })
-          home-manager.darwinModules.home-manager {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.chrislarose = import ./home.nix;
-            home-manager.extraSpecialArgs = {
-              inherit system pinpox;
-              server = false;
-            };
-          }
-        ];
+    darwinConfigurations = (
+      import ./darwin-configurations {
+        inherit nixpkgs darwin home-manager fzfVim fzfProject pinpox;
       }
     );
   };
