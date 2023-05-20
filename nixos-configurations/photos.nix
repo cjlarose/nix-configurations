d976962ee7ec58d83fc6f731d7849daf944e0506
Chris LaRose <cjlarose@gmail.com>
2023-05-20 04:40:44 +0000

    Revert "Add tailscale configuration"
    This reverts commit b140bf6378211996ea3d269cc8402e7263ad4476.

--- nixos-configurations/photos.nix
+++ nixos-configurations/photos.nix
@@ -4,7 +4,7 @@ let
 in nixpkgs.lib.nixosSystem {
   inherit system;
   modules = [
-    ({ config, pkgs, ... }: {
+    ({ pkgs, ... }: {
       imports = [ ./photos-hardware.nix ];
 
       networking.hostName = "photos";
@@ -14,20 +14,12 @@ in nixpkgs.lib.nixosSystem {
 
       system.stateVersion = "22.05";
 
-      networking.firewall = {
-        # always allow traffic from your Tailscale network
-        trustedInterfaces = [ "tailscale0" ];
-
-        # allow the Tailscale UDP port through the firewall
-        allowedUDPPorts = [ config.services.tailscale.port ];
-
-        allowedTCPPorts = [
-          80 # ingress-nginx
-          443 # ingress-nginx
-          6443 # k8s API
-          10250 # k8s node API
-        ];
-      };
+      networking.firewall.allowedTCPPorts = [
+        80 # ingress-nginx
+        443 # ingress-nginx
+        6443 # k8s API
+        10250 # k8s node API
+      ];
 
       nix = {
         package = pkgs.nixFlakes;
@@ -55,37 +47,7 @@ in nixpkgs.lib.nixosSystem {
       environment.systemPackages = with pkgs; [
         iotop
         lsof
-        tailscale
       ];
-
-      services.tailscale.enable = true;
-
-      systemd.services.tailscale-autoconnect = {
-        description = "Automatic connection to Tailscale";
-
-        # make sure tailscale is running before trying to connect to tailscale
-        after = [ "network-pre.target" "tailscale.service" ];
-        wants = [ "network-pre.target" "tailscale.service" ];
-        wantedBy = [ "multi-user.target" ];
-
-        # set this service as a oneshot job
-        serviceConfig.Type = "oneshot";
-
-        # have the job run this shell script
-        script = with pkgs; ''
-          # wait for tailscaled to settle
-          sleep 2
-
-          # check if we are already authenticated to tailscale
-          status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
-          if [ $status = "Running" ]; then # if so, then do nothing
-            exit 0
-          fi
-
-          # otherwise fail
-          exit 1
-        '';
-      };
 
       services.openiscsi = {
         enable = true;
