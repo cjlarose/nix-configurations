661180dfce5e0bf8db434a176b02db610b2e45c4
Chris LaRose <cjlarose@gmail.com>
2023-06-07 17:11:59 -0700

    Update to nixpkgs release 23.05

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "NixOS-based development environment";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-22.05";
+    nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
     darwin.url = "github:lnl7/nix-darwin/master";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager/release-22.05";
+    home-manager.url = "github:nix-community/home-manager/release-23.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     fzfVim.url = "github:cjlarose/fzf.vim";
     fzfVim.inputs.nixpkgs.follows = "nixpkgs";
