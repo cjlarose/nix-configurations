d30e3ccc55d05266d8e1b3be44fa48b2ed5b7f1b
Chris LaRose <cjlarose@gmail.com>
2023-11-30 00:54:51 +0000

    Reorganize flake inputs

--- flake.nix
+++ flake.nix
@@ -2,16 +2,28 @@
   description = "NixOS-based development environment";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
-    darwin.url = "github:lnl7/nix-darwin/master";
-    darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager/release-23.05";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    fzfVim.url = "github:cjlarose/fzf.vim";
-    fzfVim.inputs.nixpkgs.follows = "nixpkgs";
-    fzfProject.url = "github:cjlarose/fzf-project";
-    fzfProject.inputs.nixpkgs.follows = "nixpkgs";
-    tfenv.url = "github:cjlarose/tfenv-nix";
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/release-23.05";
+    };
+    darwin = {
+      url = "github:lnl7/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    fzfVim = {
+      url = "github:cjlarose/fzf.vim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    fzfProject = {
+      url = "github:cjlarose/fzf-project";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    tfenv = {
+      url = "github:cjlarose/tfenv-nix";
+    };
     nixos-generators = {
       url = "github:nix-community/nixos-generators";
       inputs.nixpkgs.follows = "nixpkgs";
