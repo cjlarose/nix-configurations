7682a22ede750229a2ce562726e70d6966ec9bb1
Chris LaRose <cjlarose@gmail.com>
2024-01-15 23:19:34 +0000

    Add configuration for nixremote user on builder

--- nixos-configurations/builder/configuration.nix
+++ nixos-configurations/builder/configuration.nix
@@ -13,6 +13,11 @@
       experimental-features = nix-command flakes
     '';
     registry.nixpkgs.flake = nixpkgs;
+    settings = {
+      trusted-users = [
+        "nixremote"
+      ];
+    };
   };
 
   nixpkgs.overlays = sharedOverlays;
@@ -46,6 +51,14 @@
     hashedPassword = "$6$YLrfXTwu61JGE.v8$kR5ZdMso2lcnyy7s7GXkIb.kLDyQ2UW3aDyGerQYni96g2kPC1MIY48Y9Q3SdYe2ycuVCrKgH6DlOjUUsK02s0";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGFtA/9w60OssA+Eji+Ygvd1XCJk/zw/uYLdiiaevELu cjlarose"
+    ];
+  };
+
+  users.users.nixremote = {
+    isNormalUser = true;
+    home = "/home/nixremote";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPp4cKoDeaCPBbAjb1csqhkrthnxhI1Nc7oDJrgukC/u root@bots"
     ];
   };
 }
