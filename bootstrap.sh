34c0fe3f89b66a5778a4ba4961e37703c9f1659b
Chris LaRose <cjlarose@gmail.com>
2024-01-29 21:56:07 -0800

    Remove swap partition in new VMs

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,33 +4,26 @@ set -e
 
 partition_and_format() {
   local blockdevice=$1
-  parted "${blockdevice}" -- mklabel gpt
-
-  # root partition starts after the boot partition and goes until the swap partition
-  parted "${blockdevice}" -- mkpart primary 512MiB -8GiB
-
-  # 8GiB swap partition at the end
-  parted "${blockdevice}" -- mkpart primary linux-swap -8GiB 100%
+  parted -s "${blockdevice}" -- mklabel gpt
 
   # 512MiB boot partition at the beginning
   parted "${blockdevice}" -- mkpart ESP fat32 1MiB 512MiB
-  parted "${blockdevice}" -- set 3 esp on
+  parted "${blockdevice}" -- set 1 esp on
 
-  # initialize ext4 filesystem and label it
-  mkfs.ext4 -L nixos "${blockdevice}1"
-
-  # initialize swap
-  mkswap -L swap "${blockdevice}2"
+  # root partition starts after the boot partition and goes until the end of the disk
+  parted "${blockdevice}" -- mkpart primary 512MiB 100%
 
   # boot partition
-  mkfs.fat -F 32 -n boot "${blockdevice}3"
+  mkfs.fat -F 32 -n boot "${blockdevice}1"
+
+  # initialize ext4 filesystem and label it
+  mkfs.ext4 -L nixos "${blockdevice}2"
 }
 
 mount_block_devices() {
   mount /dev/disk/by-label/nixos /mnt
   mkdir -p /mnt/boot
   mount /dev/disk/by-label/boot /mnt/boot
-  swapon /dev/disk/by-label/swap
 }
 
 write_nixos_config() {
