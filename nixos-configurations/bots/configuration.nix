a24214c5280ddf15de51d7084f861a215dc0429a
Chris LaRose <cjlarose@gmail.com>
2024-02-05 06:08:20 +0000

    Set explicit uids for users in bots config
    It uses external storage, so we want the uids to be consistent across
    rebuilds

--- nixos-configurations/bots/configuration.nix
+++ nixos-configurations/bots/configuration.nix
@@ -96,19 +96,23 @@
 
   users.mutableUsers = false;
 
-  users.users.cjlarose = {
-    isNormalUser = true;
-    home = "/home/cjlarose";
-    extraGroups = [ "docker" "wheel" ];
-    shell = pkgs.zsh;
-    hashedPassword = "$6$YLrfXTwu61JGE.v8$kR5ZdMso2lcnyy7s7GXkIb.kLDyQ2UW3aDyGerQYni96g2kPC1MIY48Y9Q3SdYe2ycuVCrKgH6DlOjUUsK02s0";
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGFtA/9w60OssA+Eji+Ygvd1XCJk/zw/uYLdiiaevELu cjlarose"
-    ];
-  };
+  users.users = {
+    cjlarose = {
+      uid = 1000;
+      isNormalUser = true;
+      home = "/home/cjlarose";
+      extraGroups = [ "docker" "wheel" ];
+      shell = pkgs.zsh;
+      hashedPassword = "$6$YLrfXTwu61JGE.v8$kR5ZdMso2lcnyy7s7GXkIb.kLDyQ2UW3aDyGerQYni96g2kPC1MIY48Y9Q3SdYe2ycuVCrKgH6DlOjUUsK02s0";
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGFtA/9w60OssA+Eji+Ygvd1XCJk/zw/uYLdiiaevELu cjlarose"
+      ];
+    };
 
-  users.users.bot = {
-    isNormalUser = true;
-    home = "/home/bot";
+    bot = {
+      uid = 1001;
+      isNormalUser = true;
+      home = "/home/bot";
+    };
   };
 }
