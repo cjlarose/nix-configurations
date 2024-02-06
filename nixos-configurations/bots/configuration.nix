effceeca740ed62cd8c3f5e8e79b96ec2b5dd8b4
Chris LaRose <cjlarose@gmail.com>
2024-02-06 02:59:04 +0000

    Load SSH host keys from persistent storage

--- nixos-configurations/bots/configuration.nix
+++ nixos-configurations/bots/configuration.nix
@@ -39,6 +39,17 @@
       PasswordAuthentication = false;
       PermitRootLogin = "no";
     };
+    hostKeys = [
+      {
+        path = "/persistence/ssh/ssh_host_ed25519_key";
+        type = "ed25519";
+      }
+      {
+        path = "/persistence/ssh/ssh_host_rsa_key";
+        type = "rsa";
+        bits = 4096;
+      }
+    ];
   };
 
   systemd.services."tigervnc-server" = {
