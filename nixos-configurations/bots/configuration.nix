e7fd0dd0210201b7e5d03b003010754d0133c58b
Chris LaRose <cjlarose@gmail.com>
2024-02-24 20:45:11 +0000

    Add nginx server to proxy to rails
    Configured to use ACME certificates and to proxy to localhost:3000,
    where we expect Rails to be listening

--- nixos-configurations/bots/configuration.nix
+++ nixos-configurations/bots/configuration.nix
@@ -85,6 +85,26 @@
     dataDir = "/persistence/postgresql";
   };
 
+  services.nginx = {
+    enable = true;
+    virtualHosts = {
+      "bots.toothyshouse.com" = {
+        enableACME = true;
+        acmeRoot = null;
+        forceSSL = true;
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:3000";
+          extraConfig = ''
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto $scheme;
+          '';
+        };
+      };
+    };
+  };
+
   systemd.services."tigervnc-server" = {
     description = "TigerVNC Server";
     wantedBy = [ "multi-user.target" ];
