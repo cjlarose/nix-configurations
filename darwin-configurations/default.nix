112291bcfd2e0434c8b8a376bcaacd091c115a3b
Chris LaRose <cjlarose@gmail.com>
2024-03-13 20:56:13 -0700

    Move darwin system configuration to new dir

--- darwin-configurations/default.nix
+++ darwin-configurations/default.nix
@@ -1,53 +1,7 @@
-{ nixpkgs, sharedOverlays, additionalPackages, darwin, home-manager }:
-let
-  system = "x86_64-darwin";
-  stateVersion = "23.05";
-in {
-  "LaRose-MacBook-Pro" = darwin.lib.darwinSystem {
-    inherit system;
-    modules = [
-      ({ ... }: {
-        environment.etc = {
-          "sysctl.conf" = {
-            enable = true;
-            text = ''
-              kern.maxfiles=131072
-              kern.maxfilesperproc=65536
-            '';
-          };
-        };
-        services.nix-daemon.enable = true;
-        programs.zsh.enable = true;
-        system.stateVersion = 4;
-        nix = {
-          extraOptions = ''
-            experimental-features = nix-command flakes
-          '';
-          registry.nixpkgs.flake = nixpkgs;
-        };
-        nixpkgs = {
-          overlays = sharedOverlays ++ [
-            (final: prev: {
-              nodejs = nixpkgs.legacyPackages.${system}.nodejs_20;
-            })
-          ];
-          config.allowUnfreePredicate = pkg: builtins.elem (nixpkgs.lib.getName pkg) [
-            "1password-cli"
-          ];
-        };
-        users.users.chrislarose = {
-          home = "/Users/chrislarose";
-        };
-      })
-      home-manager.darwinModules.home-manager {
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-        home-manager.users.chrislarose = import ../home;
-        home-manager.extraSpecialArgs = {
-          inherit system stateVersion additionalPackages;
-          server = false;
-        };
-      }
-    ];
-  };
+{ nixpkgs, sharedOverlays, additionalPackages, darwin, home-manager }: {
+  "LaRose-MacBook-Pro" = (
+    import ./larose-mbp {
+      inherit additionalPackages darwin home-manager nixpkgs sharedOverlays;
+    }
+  );
 }
