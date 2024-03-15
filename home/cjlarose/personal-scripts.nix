3fd71e0679ce098164f7fd65700a1396135b1373
Chris LaRose <cjlarose@gmail.com>
2024-03-15 13:20:24 -0700

    Add true color test script

--- home/cjlarose/personal-scripts.nix
+++ home/cjlarose/personal-scripts.nix
@@ -24,11 +24,27 @@ let
   runUntilSuccess = pkgs.writeShellScriptBin "run-until-success" ''
     while ! "$@"; do :; done
   '';
+
+  trueColorTest = pkgs.stdenv.mkDerivation {
+    name = "true-color-test";
+    src = fetchGit {
+      url = "https://gist.github.com/09a44b8cf0f9397465614e622979107f.git";
+      rev = "d89f28711c037e53f03e312f6fef11bcc75006f8";
+    };
+    buildPhase = ''
+      chmod +x 24-bit-color.sh
+    '';
+    installPhase = ''
+      mkdir -p $out/bin
+      cp 24-bit-color.sh $out/bin
+    '';
+  };
 in {
   home.packages = [
     nvrEditInSplitWindow
     wrappedRg
     runUntilFailure
     runUntilSuccess
+    trueColorTest
   ];
 }
