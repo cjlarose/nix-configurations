d11e1e9cec28c602e38c7caf25697d44a445ceb0
Chris LaRose <cjlarose@gmail.com>
2024-05-10 21:37:35 -0700

    Explicitly set shell for cjlarose user

--- nixos-configurations/bootstrap/configuration.nix
+++ nixos-configurations/bootstrap/configuration.nix
@@ -58,6 +58,7 @@
   users.users.cjlarose = {
     isNormalUser = true;
     home = "/home/cjlarose";
+    shell = pkgs.zsh;
     extraGroups = [ "wheel" ];
     hashedPassword = "$6$YLrfXTwu61JGE.v8$kR5ZdMso2lcnyy7s7GXkIb.kLDyQ2UW3aDyGerQYni96g2kPC1MIY48Y9Q3SdYe2ycuVCrKgH6DlOjUUsK02s0";
     openssh.authorizedKeys.keys = [
