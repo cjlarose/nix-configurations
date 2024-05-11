7aef0f6a6bfb3004a9d707ef8ef784f114068f3a
Chris LaRose <cjlarose@gmail.com>
2024-05-10 21:37:18 -0700

    Store SSH host keys on bootstrap in persistent filesystem

--- nixos-configurations/bootstrap/configuration.nix
+++ nixos-configurations/bootstrap/configuration.nix
@@ -36,6 +36,17 @@
       PasswordAuthentication = false;
       PermitRootLogin = "no";
     };
+    hostKeys = [
+      {
+        path = "/persistence/ssh/ssh_host_ed25519_key";
+        type = "ed25519";
+      }
+      {
+        path = "/persistence/ssh/ssh_host_rsa_key";
+        type = "rsa";
+        bits = 4096;
+      }
+    ];
   };
 
   programs.ssh.startAgent = true;
