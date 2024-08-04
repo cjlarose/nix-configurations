82605f3d3e47375fa6b21cd74e87cb62e340ff4e
Chris LaRose <cjlarose@gmail.com>
2024-08-04 06:17:13 +0000

    Load swap device on bots
    I extended the VM disk and created the partition manually

--- nixos-configurations/bots/hardware-configuration.nix
+++ nixos-configurations/bots/hardware-configuration.nix
@@ -31,7 +31,9 @@
       fsType = "ext4";
     };
 
-  swapDevices = [ ];
+  swapDevices = [
+    { device = "/dev/disk/by-label/swap"; }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
