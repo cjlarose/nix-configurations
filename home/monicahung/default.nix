eab7aad9f1d89522d6341e855d8093045a61463a
Monica Hung <monica.hung11@gmail.com>
2024-09-29 14:23:32 -0700

    Add 1password ssh agent

--- home/monicahung/default.nix
+++ home/monicahung/default.nix
@@ -90,6 +90,7 @@
     extraOptionOverrides = {
       AddKeysToAgent = "yes";
     };
+    extraConfig = "IdentityAgent \"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
   };
 
   programs.fzf = {
