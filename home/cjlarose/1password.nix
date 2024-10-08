08318c92cb0f272c6b8475b9fa8f29cbfc8da742
Chris LaRose <cjlarose@gmail.com>
2024-10-07 23:13:41 -0700

    Enable commit signing

--- home/cjlarose/1password.nix
+++ home/cjlarose/1password.nix
@@ -4,6 +4,19 @@
     pkgs._1password
   ];
 
+  programs.git = {
+    signing = {
+      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPVpeUiVCUdL3/2xAORyus00XAOrvXukwpOiaZhdHoKs";
+      signByDefault = true;
+    };
+    extraConfig = {
+      gpg.format = "ssh";
+      "gpg \"ssh\"" = {
+        program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+      };
+    };
+  };
+
   programs.ssh = {
     extraConfig = ''
       IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
