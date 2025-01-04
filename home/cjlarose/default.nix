7eda4ae710b6b668eee2d63076535bc80a76bd96
Chris LaRose <cjlarose@gmail.com>
2025-01-04 19:32:10 +0000

    Replace pkgs.nodePackages.pyright with pkgs.pyright
    Both seem to be available on nixpkgs 24.05, but only the latter is
    available on nixpkgs 24.11.

--- home/cjlarose/default.nix
+++ home/cjlarose/default.nix
@@ -49,7 +49,7 @@
       pkgs.neovim-remote
       pkgs.nil
       pkgs.nodePackages.bash-language-server
-      pkgs.nodePackages.pyright
+      pkgs.pyright
       pkgs.nodePackages.typescript-language-server
       pkgs.nodePackages.vscode-langservers-extracted
       pkgs.nodejs_20
