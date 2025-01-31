09a099d38a4181feb4cc1b0cee391150a33d63a1
Chris LaRose <cjlarose@gmail.com>
2025-01-31 01:04:52 +0000

    Add postgresql service

--- nixos-configurations/splitpro/configuration.nix
+++ nixos-configurations/splitpro/configuration.nix
@@ -53,6 +53,25 @@
     ];
   };
 
+  services.postgresql = {
+    enable = true;
+    enableTCPIP = true;
+    authentication = ''
+      # Allow any user on the local system to connect to any database with
+      # any database user name using Unix-domain sockets (the default for local
+      # connections).
+      #
+      # TYPE  DATABASE        USER            ADDRESS                 METHOD
+      local   all             all                                     trust
+
+      # Require password authentication when accessing over TCP/IP, all addresses
+      #
+      # TYPE  DATABASE        USER            ADDRESS                 METHOD
+      host    all             all             0.0.0.0/0               scram-sha-256
+    '';
+    dataDir = "/persistence/postgresql";
+  };
+
   services.tailscale = {
     enable = true;
   };
