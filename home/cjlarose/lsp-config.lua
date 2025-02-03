e57135c99137f59c1eaffea1cd49a3eacbe82cc5
Chris LaRose <cjlarose@gmail.com>
2025-02-03 10:12:38 -0800

    Remove binding for formatting
    This was causing errors when attaching to buffers like
    
       LSP[pywright]: Error ON_ATTACH_ERROR: "vim/keymap.lua:0: rhs: expected string|function, got nil"

--- home/cjlarose/lsp-config.lua
+++ home/cjlarose/lsp-config.lua
@@ -29,7 +29,6 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
   vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>ft', vim.lsp.buf.formatting, bufopts)
 end
 
 require('lspconfig')['gopls'].setup{
