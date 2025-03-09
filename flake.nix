2e4c83f0c1246ce96bb4b4dd7827d6f3f5350e85
Chris LaRose <cjlarose@gmail.com>
2025-03-09 08:36:51 -0700

    Move git-make-apply-command derivation to its own file

--- flake.nix
+++ flake.nix
@@ -105,35 +105,7 @@
           chicken-smoothie-automation = chicken-smoothie-automation.packages.${system}.default;
           bundix = import "${bundix}/default.nix" { inherit pkgs; };
           intranetHosts = intranetHosts;
-          git-make-apply-command = pkgs.writeShellScriptBin "git-make-apply-command" ''
-            # This script is used to generate a command that can be used to apply a diff
-            # The diff is passed to this script via stdin
-            # The generated command is printed to stdout
-
-            # Start the command
-            printf '%b' 'printf %b $'"'"
-
-            # Process input one byte at a time
-            while IFS= read -r -d ''' -n1 char || [[ -n "$char" ]]; do
-                # Get ASCII value
-                LC_CTYPE=C printf -v ord "%d" "'$char"
-
-                # Handle different character types
-                if [[ "$char" == '\' ]]; then
-                    # Escape backslashes
-                    printf '%s' '\\\\'
-                elif [[ "$ord" -ge 32 && "$ord" -le 126 ]] || [[ "$ord" -eq 10 ]]; then
-                    # Printable characters are kept as-is
-                    printf '%s' "$char"
-                else
-                    # Encode other characters as \xHH
-                    printf '\\\\x%02x' "$ord"
-                fi
-            done
-
-            # Close the command
-            printf '%b' "'"' | git apply -\n'
-          '';
+          git-make-apply-command = import ./packages/git-make-apply-command { inherit pkgs; };
           nix-direnv = nixpkgs-24-11.legacyPackages.${system}.nix-direnv;
           nvr = let
             manifest = (pkgs.lib.importTOML "${nvr.outPath}/Cargo.toml").package;
---
+++ packages/git-make-apply-command/default.nix
@@ -0,0 +1,31 @@
+{ pkgs }:
+
+pkgs.writeShellScriptBin "git-make-apply-command" ''
+  # This script is used to generate a command that can be used to apply a diff
+  # The diff is passed to this script via stdin
+  # The generated command is printed to stdout
+
+  # Start the command
+  printf '%b' 'printf %b $'"'"
+
+  # Process input one byte at a time
+  while IFS= read -r -d ''' -n1 char || [[ -n "$char" ]]; do
+      # Get ASCII value
+      LC_CTYPE=C printf -v ord "%d" "'$char"
+
+      # Handle different character types
+      if [[ "$char" == '\' ]]; then
+          # Escape backslashes
+          printf '%s' '\\\\'
+      elif [[ "$ord" -ge 32 && "$ord" -le 126 ]] || [[ "$ord" -eq 10 ]]; then
+          # Printable characters are kept as-is
+          printf '%s' "$char"
+      else
+          # Encode other characters as \xHH
+          printf '\\\\x%02x' "$ord"
+      fi
+  done
+
+  # Close the command
+  printf '%b' "'"' | git apply -\n'
+''
