6069533c73a0f56fdf3c18f05be0d84d59b99358
Chris LaRose <cjlarose@gmail.com>
2025-04-27 18:02:42 -0700

    Add immich config

---
+++ nixos-configurations/immich/hardware-configuration.nix
@@ -0,0 +1,52 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports =
+    [ (modulesPath + "/profiles/qemu-guest.nix")
+    ];
+
+  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_blk" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "tank/root";
+      fsType = "zfs";
+    };
+
+  fileSystems."/nix" =
+    { device = "tank/nix";
+      fsType = "zfs";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-partlabel/disk-main-ESP";
+      fsType = "vfat";
+    };
+
+  fileSystems."/persistence" =
+    { device = "/dev/disk/by-partlabel/persistence";
+      neededForBoot = true;
+      fsType = "ext4";
+    };
+
+  fileSystems."/library" =
+    { device = "/dev/disk/by-partlabel/library";
+      neededForBoot = true;
+      fsType = "ext4";
+    };
+
+  swapDevices = [
+    { device = "/dev/disk/by-partlabel/disk-main-swap"; }
+  ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.ens18.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+}
