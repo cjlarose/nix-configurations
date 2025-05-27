bf18987569f721a543411a81541dbbcb5ac3f3ba
Chris LaRose <cjlarose@gmail.com>
2025-05-26 17:55:27 -0700

    [dns] Remove unused proxmox image config

--- nixos-configurations/dns/default.nix
+++ nixos-configurations/dns/default.nix
@@ -34,27 +34,6 @@ in nixpkgs.lib.nixosSystem {
         };
       };
     })
-    ({ config, pkgs, ... }: {
-      imports = [
-        ../../nixos-modules/proxmox-image.nix
-      ];
-
-      proxmox = {
-        qemuConf = {
-          name = "nixos-${config.networking.hostName}";
-          net0 = "virtio=00:00:00:00:00:00,bridge=vmbr1,firewall=1";
-          bios = "ovmf";
-          boot = "order=virtio0";
-          virtio0 = "montero-vm-storage-lvm:vm-9999-disk-0";
-          cores = 4;
-          memory = 4096;
-        };
-        qemuExtraConf = {
-          localtime = "false";
-          sockets = 1;
-        };
-      };
-    })
     (import ./configuration.nix { inherit nixpkgs sharedOverlays stateVersion system additionalPackages; })
     home-manager.nixosModules.home-manager {
       home-manager.useGlobalPkgs = true;
