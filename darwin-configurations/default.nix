9fd591934bedc22ddee079521dffe8190177e253
Chris LaRose <cjlarose@gmail.com>
2025-07-05 16:52:05 -0700

    Add configuration for new work laptop

--- darwin-configurations/default.nix
+++ darwin-configurations/default.nix
@@ -9,6 +9,16 @@
   home-manager-24-11,
   home-manager-25-05
 }: {
+  "Christophers-MacBook-Pro" = (
+    import ./larose-mbp {
+      inherit additionalPackages darwin sharedOverlays;
+      nixpkgs = nixpkgs-25-05;
+      home-manager = home-manager-25-05;
+      primaryUser = "cjlarose";
+      homeManagerStateVersion = "25.05";
+      nixDarwinStateVersion = 6;
+    }
+  );
   "LaRose-MacBook-Pro" = (
     import ./larose-mbp {
       inherit additionalPackages darwin sharedOverlays;
